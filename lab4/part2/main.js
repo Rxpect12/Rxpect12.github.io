f2fd866f556670889f592723702942c44a60b2a6
Rxpect12 <benjamin.stonge@dcmail.ca>
2025-03-05 13:28:35 -0500

    Source code

---
+++ lab4/part2/main.js
@@ -0,0 +1,21 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const pics = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+const altpics = [pic1 = "Close up of eyes", pic2 = "Cool rock formation", pic3 = "Lavendar flowers", pic4 = "Egyptian art", pic5 = "Butterfly on a leaf"]
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
